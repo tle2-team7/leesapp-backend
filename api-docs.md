2039583261a433500257adb308e4b24d0369fd74
MaxvanSchoonderwoerd <maxvanschoonderwoerd@gmail.com>
2023-05-10 11:27:15 +0200

    added documentations

--- api-docs.md
+++ api-docs.md
@@ -1,2 +1,7 @@
 # API Documentatie Backend
+
 Documentatie van de API calls die mogelijk zijn vanuit de backend
+
+/chat method: POST description: Dit is het endpoint om met chatGPT te interacteren. Het heeft een start prompt en accepteerd een prompt van de user als parameter.
+
+/ method: GET description: Dit is een endpoint om de API te testen. Het geeft een status 200 en "Hello world" terug bij success.
--- server.js
+++ server.js
@@ -4,23 +4,26 @@ import config from "./config.json" assert { type: "json" };
 import { Configuration, OpenAIApi } from "openai";
 import bodyParser from "body-parser";
 
-const configuration = new Configuration({
-  apiKey: config.API_KEY,
-});
-const openai = new OpenAIApi(configuration);
-
+//express.js init and setup configurations
 const app = express();
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json({ type: "application/json" }));
 
-app.post("/chat", async (req, res) => {
+//OpenAI API init and setup configurations
+const configuration = new Configuration({
+  apiKey: config.API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+//main endpoint for interacting with chatGPT
+app.post("/start", async (req, res) => {
   try {
     const prompt = req.body.prompt;
     const response = await openai.createChatCompletion({
       model: "gpt-3.5-turbo",
       messages: [
-        { role: "system", content: "Genereer een Nederlandse zin op A1 niveau" },
+        { role: "system", content: config.START_PROMPT },
         { role: "user", content: prompt },
       ],
     });
@@ -31,20 +34,8 @@ app.post("/chat", async (req, res) => {
   }
 });
 
-app.get("/start", async (req, res) => {
-  try {
-    const prompt = req.body.prompt;
-    const response = await openai.createChatCompletion({
-      model: "gpt-3.5-turbo",
-      messages: [{ role: "system", content: "Je genereert teksten op A2 leesniveau die voorgelezen gaan worden door mensen die nederlands aan het leren lezen zijn. Je genereerd elke keer met 1 nieuwe zin, de zin die voorgelezen moet worden begin je met lees voor: [DE ZIN DIE VOORGELEZEN MOET WORDEN]. De lezer zal deze voorlezen en op basis van de fouten genereer je een nieuwe zin waar het verkeerde woord in zit" }],
-    });
-    res.status(200).send(response.data);
-  } catch (error) {
-    res.status(500).send(error);
-    console.error(error);
-  }
-});
-
+//simple get endpoint for testing API
+//can probably be deleted
 app.get("/", (req, res) => {
   try {
     res.status(200).send("Hello world");
@@ -54,6 +45,7 @@ app.get("/", (req, res) => {
   }
 });
 
-app.listen(config.port, () => {
-  console.log(`Server listening on port ${config.port}`);
+//this starts the API
+app.listen(config.PORT, () => {
+  console.log(`Server listening on port ${config.PORT}`);
 });
